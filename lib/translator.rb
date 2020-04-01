4d98d0bad795fc1b6d42195f882a6421f9f11209
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-01 02:57:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ def get_japanese_emoticon(path, w_emoticon)
       return lang_hash[:japanese]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, j_emoticon)
