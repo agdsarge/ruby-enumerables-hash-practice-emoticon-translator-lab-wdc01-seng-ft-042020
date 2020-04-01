c66835b555ffa48636eadb0c3bc7337e0c2b60f1
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-01 02:35:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,15 @@
 # require modules here
 
 def load_library(path)
-  # code goes here
-  #return hash where each key is name of emoticon
-  #each key's value is a hash. keys are :english and :japanese
+
   require 'yaml'
-  require 'pp'
+
   top = YAML.load_file(path)
-  pp top
   erg = {}
   top.each_pair do |name, emot_array|
     erg[name] = {english: emot_array[0], japanese: emot_array[1]}
   end
-  pp erg
+
   return erg
 end
 
