c67470bb624c1c11246ecce3f7a2f47062aede13
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-01 02:39:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,25 +1,23 @@
 # require modules here
 
 def load_library(path)
-
   require 'yaml'
-
   top = YAML.load_file(path)
   dict = {}
   top.each_pair do |name, emot_array|
     dict[name] = {english: emot_array[0], japanese: emot_array[1]}
   end
-
   return dict
 end
 
 
-
-
 def get_japanese_emoticon
-  # code goes here
+  
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, w_emoticon)
+  dictionary = load_library(path)
+  pp dictionary
+  
 end
