3a54ecebcd7d358d96014c7d7527d3e3a0f10404
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-01 02:43:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,9 @@ def get_english_meaning(path, w_emoticon)
   require 'pp'
   dictionary = load_library(path)
   pp dictionary
+  dictionary.each_pair do |meaning, lang_hash|
+    lang_hash.each_pair do |language. emot|
+      pp emot
   return nil
 end
 
