a35ff496bf7c376be974f7c0469009ec208adf52
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-01 02:29:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,15 @@
 
 def load_library
   # code goes here
+  #return hash where each key is name of emoticon
+  #each key's value is a hash. keys are :english and :japanese
+  require 'yaml'
+  require 'pp'
+  top = YAML.load_file('emoticons.yml')
+  pp top
+  
+  
+  
 end
 
 def get_japanese_emoticon
